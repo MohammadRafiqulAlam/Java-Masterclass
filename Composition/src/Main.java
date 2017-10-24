315c07fe971bcf1df5f3f73f7d3ca3c96255711a
MohammadRafiqulAlam <m.rafiqulalam@yahoo.coom>
2017-10-24 13:48:14 +0800

    Version Date: 10/24/2017

---
+++ Composition/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static   void main(String[] args){
+
+        Dimensions dimensions = new Dimensions(20,20,5);
+        Case theCase = new Case("220B","Dell","240",dimensions);
+        Monitor monitor = new Monitor("27 Inch", "Acer",27,new Resolution(2540,1440));
+        Motherboard themotherBoard = new Motherboard("1234","Intel",2,4,"2.3" );
+        // The PC is composed of case, monitor and mother board
+        PC thePC =new PC(theCase,monitor,themotherBoard);
+        thePC.getMonitor().drawPixelAt(100,200,"Red");
+    }
+}
---
+++ Composition/src/PC.java
@@ -0,0 +1,25 @@
+public class PC {
+    // Composition example
+    // The PC has the objects of Case, Monitor, and Motherboard
+    private Case theCase;
+    private Monitor monitor;
+    private Motherboard motherboard;
+
+    public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
+        this.theCase = theCase;
+        this.monitor = monitor;
+        this.motherboard = motherboard;
+    }
+
+    public Case getTheCase() {
+        return theCase;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public Motherboard getMotherboard() {
+        return motherboard;
+    }
+}
